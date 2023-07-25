4ca0284d7e312553bc18526f16d7705cb8dd9b47
Ekaterina Krivich <ekaterinak@heathmont.net>
2023-07-25 14:58:20 +0300

    add nesletter functionality

---
+++ app/api/email/route.ts
@@ -0,0 +1,47 @@
+import { google } from "googleapis";
+
+type SheetForm = {
+    email: string;
+}
+
+export async function POST(request: Request) {
+    const body = await request.json() as SheetForm;
+
+    try {
+        console.log('POST accepted');
+        const auth = new google.auth.GoogleAuth({
+            credentials: {
+                client_email: process.env.GOOGLE_CLIENT_EMAIL,
+                private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+            },
+            scopes: [
+                'https://www.googleapis.com/auth/drive',
+                'https://www.googleapis.com/auth/drive.file',
+                'https://www.googleapis.com/auth/spreadsheets'
+            ],
+        });
+
+        const sheets = google.sheets({ version: 'v4', auth });
+
+        const response = await sheets.spreadsheets.values.append({
+            spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
+            range: 'Sheet1!A1',
+            valueInputOption: 'USER_ENTERED',
+            requestBody: {
+                values: [
+                    [body.email]
+                ]
+            }
+        });
+
+        return new Response(JSON.stringify(response.data), {
+            headers: { 'content-type': 'application/json' },
+        });
+    } catch (error) {
+        console.error(error);
+        return new Response(JSON.stringify(error), {
+            headers: { 'content-type': 'application/json' },
+        });
+    }
+}
+  
--- components/newsletter.tsx
+++ components/newsletter.tsx
@@ -1,4 +1,27 @@
+"use client";
+import { FormEvent, useState } from "react";
+
 export default function Newsletter() {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    const form = { email };
+
+    const response = await fetch('/api/email', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(form),
+    });
+
+    const content = await response.json();
+
+    console.log(content);
+
+    setEmail('');
+  }
+
   return (
     <div className="relative text-center px-4 py-6 group">
       <div
@@ -7,15 +30,15 @@ export default function Newsletter() {
       />
       <div className="font-nycd text-xl text-indigo-500 mb-1">Sign up now and let our referral system work for you!</div>
       <div className="text-2xl font-bold mb-5">We will notify you when we are open</div>
-      <form className="inline-flex max-w-sm">
+      <form className="inline-flex max-w-sm" onSubmit={handleSubmit}>
         <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-none">
-          <input type="email" className="form-input py-1.5 w-full mb-2 sm:mb-0 sm:mr-2" placeholder="Your email" aria-label="Your email" />
+          <input value={email} onChange={e => setEmail(e.target.value)} type="email" className="form-input py-1.5 w-full mb-2 sm:mb-0 sm:mr-2" placeholder="Your email" aria-label="Your email" />
           <button className="btn-sm text-white bg-indigo-500 hover:bg-indigo-600 shadow-sm whitespace-nowrap" type="submit">
             Notify me
           </button>
         </div>
         {/* Success message */}
-        {/* <p className="font-medium text-emerald-600 text-center sm:text-left sm:absolute mt-2 opacity-75 text-sm">Thanks for subscribing!</p> */}
+        <p className="font-medium text-emerald-600 text-center sm:text-left sm:absolute mt-2 opacity-75 text-sm">Thanks for subscribing!</p>
       </form>
     </div>
   )
